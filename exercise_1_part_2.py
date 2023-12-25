aa469b289b939207e81f496e4b7ef762ff6d24f2
Max Arkell <maxarkell@icloud.com>
2023-12-25 11:56:05 +0000

    still wont work

--- exercise_1_part_2.py
+++ exercise_1_part_2.py
@@ -1002,7 +1002,7 @@ one1onermlsevenseven
 """
 my_list = big_list.splitlines()
 number_strings =['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-numbers =[0,1,2,3,4,5,6,7,8,9]
+numbers =['0', '1' ,'2' ,'3' ,'4', '5' ,'6' ,'7' ,'8' ,'9']
 my_altered_list = []
 def number_grab(a):
     numbers_only=''.join(filter(str.isdigit, a))
@@ -1011,7 +1011,7 @@ def number_grab(a):
 def string_swap(b):
     for item in number_strings:
         position = number_strings.index(item)
-        if item in b:
+        if str(item) in b:
             b.replace(str(item), str(numbers[position]))
     return b
 
